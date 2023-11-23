ef249a7b913f98ae8689307acaa43240fe25d95f
Bruno <brunonasccimentto@gmail.com>
2023-11-23 19:20:33 -0300

    ✅: add fetchBookList and file

---
+++ lib/models/book/book_repository.dart
@@ -0,0 +1,19 @@
+import 'package:escribo_ebook/models/book/book.dart';
+import 'package:escribo_ebook/models/book/services/api/api.dart';
+
+class BookRepository {
+  final bookApi = BookApiService();
+
+  Future<List<BookModel>> fetchBookList() async {
+    dynamic response = await bookApi.getResponse();
+    final jsonData = response as List;
+    List<BookModel> bookList = jsonData.map((json) => BookModel.fromJson(json)).toList();
+    return bookList;
+  }
+
+  Future<String> fetchFile(String url, String filename) async {
+    String response = await bookApi.getFile(url, filename);
+    return response;
+  }
+
+}
