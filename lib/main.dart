bc919e7aa98c15a07993b9d49982c72dec2effdc
Bruno <brunonasccimentto@gmail.com>
2023-11-23 19:31:40 -0300

    rename import file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:escribo_ebook/view-models/system_info_viewmodel.dart';
 import 'package:escribo_ebook/view/favorites/favorites.dart';
-import 'package:escribo_ebook/view/homescreen/homepage.dart';
+import 'package:escribo_ebook/view/homescreen/homescreen.dart';
 import 'package:escribo_ebook/view-models/book_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
