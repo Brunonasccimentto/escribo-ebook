9864edbcba4b77d375a0c893ebcac0a99f551c05
Bruno <brunonasccimentto@gmail.com>
2023-11-23 19:17:12 -0300

    ✅: add isarDatabase configs

---
+++ lib/models/book/services/database/database.dart
@@ -0,0 +1,27 @@
+import 'package:escribo_ebook/models/book/book.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseService {
+  static final DatabaseService _singleton = DatabaseService._internal();
+
+  factory DatabaseService() {
+    return _singleton;
+  }
+
+  DatabaseService._internal();
+
+ Future<Isar> openDB() async {
+    final dir = await getApplicationDocumentsDirectory();
+
+    if (Isar.instanceNames.isEmpty) {
+      return await Isar.open(
+        [BookModelSchema],
+        inspector: true,
+        directory: dir.path,
+      );
+    }
+
+    return Future.value(Isar.getInstance());
+  }
+}
