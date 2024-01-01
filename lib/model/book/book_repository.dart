9abc2deaee2e178a9309a830b9eababc380b8545
Bruno <brunonasccimentto@gmail.com>
2023-12-31 22:16:01 -0300

    refact: add override methods

--- lib/model/book/book_repository.dart
+++ lib/model/book/book_repository.dart
@@ -1,16 +1,19 @@
 import 'package:escribo_ebook/model/book/book.dart';
+import 'package:escribo_ebook/model/book/interface/ibook.dart';
 import 'package:escribo_ebook/model/book/services/api/api.dart';
 
-class BookRepository {
+class BookRepository extends IbookRepository {
   final bookApi = BookApiService();
 
-  Future<List<BookModel>> fetchBookList() async {
+  @override
+  Future<List<BookModel>> listBooks() async {
     dynamic response = await bookApi.getResponse();
     final jsonData = response as List;
     List<BookModel> bookList = jsonData.map((json) => BookModel.fromJson(json)).toList();
     return bookList;
   }
 
+  @override
   Future<String> fetchFile(String url, String filename) async {
     String response = await bookApi.getFile(url, filename);
     return response;
