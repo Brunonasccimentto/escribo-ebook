7685bfca66cd6d99839a92678ed3e6de9e77ef4d
Bruno <brunonasccimentto@gmail.com>
2023-12-31 23:34:44 -0300

    feat: implements AppExceptions use cases in returnResponse

--- lib/model/book/services/api/api.dart
+++ lib/model/book/services/api/api.dart
@@ -1,24 +1,46 @@
 
 import 'dart:convert';
 import 'dart:io';
-import 'package:escribo_ebook/model/book/services/config.dart';
+import 'package:escribo_ebook/model/book/services/api/config.dart';
+import 'package:escribo_ebook/model/book/services/app_exception.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 
 class BookApiService extends Config {
 
-  Future getResponse() async {
+  @override
+  Future getResponse(String url) async {
     dynamic responseJson = [];
     
     try {
-      final response = await http.get(Uri.parse(baseUrl));
-      responseJson = jsonDecode(response.body);
-    } catch (e) {
-      print(e);
+      final response = await http.get(Uri.parse(baseUrl + url));
+      responseJson = returnResponse(response);
+    } on SocketException {
+       throw FetchDataException('No Internet Connection');
     }
 
     return responseJson;
+  }
+
+   dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.toString());
+      case 401:
+      case 403:
+        throw UnauthorisedException(response.body.toString());
+      case 404:
+        throw UnauthorisedException(response.body.toString());
+      case 500:
+      default:
+        throw FetchDataException(
+            'Error occured while communication with server' +
+                ' with status code : ${response.statusCode}');
+    }
   }
 
   Future<String> getFile(String url, String filename) async {
