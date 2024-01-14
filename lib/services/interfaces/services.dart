421667ff39faef1ef0eb89f8fc377d2fa2b33a46
Bruno <brunonasccimentto@gmail.com>
2024-01-14 01:32:08 -0300

    feat: new service interface and implementation

--- lib/services/interfaces/services.dart
+++ lib/services/interfaces/services.dart
@@ -1,23 +1,17 @@
 import 'dart:io';
-import 'package:escribo_ebook/services/interfaces/services.dart';
+
 import 'package:escribo_ebook/model/book/services/database/database.dart';
 import 'package:http/http.dart';
 import 'package:path_provider/path_provider.dart';
 
+abstract class IServices {
 
-class MainServices implements IServices {
-
-  @override
   final Client client = Client();
-  @override
   final HttpClient httpClient = HttpClient();
-  @override
   final File file = File('');
-  @override
   final DatabaseService db = DatabaseService();
 
-  @override
-  Future<Directory> get directory => _dirInst();  
+  Future<Directory> get directory => _dirInst();
 
   Future<Directory> _dirInst() async {
     String path = (await getApplicationDocumentsDirectory()).path; 
